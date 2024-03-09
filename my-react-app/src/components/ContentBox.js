33301c699f516160dec7f7d5780a6e77cc81f307
cmax88 <cjmax88@gmail.com>
2024-03-09 15:18:35 -0500

    reset

--- my-react-app/src/components/ContentBox.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-
-function ContentBox({ heading, setHeading }) {
-  return (
-    <input type="text" value={heading} onChange={(e) => setHeading(e.target.value)} />
-  );
-  }
-export default ContentBox;
